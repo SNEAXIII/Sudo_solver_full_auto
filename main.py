0f9d12f39624508c494f4bd1aee5bf0636a9fbfc
AlexisB <misterbalise2@gmail.com>
2023-05-06 22:10:10 +0200

    code reformat

--- main.py
+++ main.py
@@ -1,72 +1,41 @@
-from time import time,sleep
-from pyautogui import locateOnScreen,screenshot,locate
+from time import time, sleep
+from pyautogui import locateOnScreen, screenshot, locate
 from PIL import ImageGrab, Image
 import os
-debut = time()
-def screenshot(): ImageGrab.grab((386, 242, 879, 735)).save("capture.png")
 
-screenshot()
-screen = Image.open("capture.png")
+debut = time()
+
+
+def screenshoted(): ImageGrab.grab((386, 242, 879, 735)).save("capture.png")
+
+
+def is_empty(region):
+    set_couleur = set()
+    for index in range(7, 20):
+        set_couleur.add(region.getpixel((25, index)))
+    return len(set_couleur) == 1
+
+
+screen = ImageGrab.grab((386, 242, 879, 735))
 
 longueur_case = 55
-files = os.listdir(f"{os.path.dirname(os.path.abspath(__file__))}\\nombres")[::-1]
+numbers = os.listdir(f"{os.path.dirname(os.path.abspath(__file__))}\\nombres")[::-1]
 sudo_liste = []
 for y in range(9):
     sudo_liste.append([])
     for x in range(9):
-        left =x * longueur_case
-        top =y * longueur_case
+        left, top = x * longueur_case, y * longueur_case
         region = screen.crop((left, top, left + longueur_case, top + longueur_case))
-        print("___________________________________")
-        print(x, y)
-        # screenshot(region=region).show()
         a_ajouter = " "
-        for nb_fichier in files:
-            if nb_fichier in files:
-                results = locate(f"nombres/{nb_fichier}",region, confidence=0.80, grayscale=True)
-                if results is not None:
-                    a_ajouter = nb_fichier[0]
-                    print(a_ajouter)
+        if not is_empty(region):
+            for nb in numbers:
+                est_present = locate(f"nombres/{nb}", region, confidence=0.80, grayscale=True)
+                if nb in numbers and est_present is not None:
+                    a_ajouter = nb[0]
                     break
         sudo_liste[y].append(a_ajouter)
-            # elif nb_fichier in files[:3]:
-            #     results = locateOnScreen(f"nombres/{nb_fichier}", region=region)
-            #     if results is not None:
-            #         sudo_liste[y].append(" ")
-            #         break
 
 for ligne in sudo_liste:
-    print(len(ligne),ligne)
-
-""" # v1
-result = reader.readtext(image, allowlist="0123456789")
-nb_a_classer = result[0][0]
-espace_entre_case = 500/9
-print(espace_entre_case)
-droite = nb_a_classer[1][0]
-gauche = nb_a_classer[0][0]
-bas = nb_a_classer[2][1]
-haut = nb_a_classer[1][1]
-
-case = (floor((droite-gauche)/espace_entre_case),floor((bas-haut)/espace_entre_case))
-print((droite,gauche),(droite-gauche),espace_entre_case/(droite-gauche))
-print((bas,haut),(bas-haut),espace_entre_case/(bas-haut))
-print(case,result[0][1])
-"""
-""" # V2
-result = reader.readtext(image, allowlist="0123456789")
-for index in range(len(result)):
-    nb_a_classer = result[index][0]
-    espace_entre_case = 500 / 9
-    droite = nb_a_classer[1][0]
-    gauche = nb_a_classer[0][0]
-    bas = nb_a_classer[2][1]
-    haut = nb_a_classer[1][1]
-
-    case = (floor((droite - gauche) / espace_entre_case), floor((bas - haut) / espace_entre_case))
-    # print((droite,gauche),(droite-gauche),espace_entre_case/(droite-gauche))
-    # print((bas,haut),(bas-haut),espace_entre_case/(bas-haut))
-    print(case,result[index][1])
-"""
+    print(len(ligne), ligne)
 
 print(f"{time() - debut} s")
