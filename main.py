38a08e78b71d03a273972727a8d312b4b735223f
AlexisB <misterbalise2@gmail.com>
2023-05-06 20:11:16 +0200

    efficiency upgrade

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 from time import time,sleep
-from pyautogui import locateOnScreen,screenshot
+from pyautogui import locateOnScreen,screenshot,locate
 from PIL import ImageGrab, Image
 import os
 debut = time()
 def screenshot(): ImageGrab.grab((386, 242, 879, 735)).save("capture.png")
 
 screenshot()
+screen = Image.open("capture.png")
 
-x_base = 386
-y_base = 242
 longueur_case = 55
 files = os.listdir(f"{os.path.dirname(os.path.abspath(__file__))}\\nombres")[::-1]
 sudo_liste = []
 for y in range(9):
     sudo_liste.append([])
     for x in range(9):
-        left = x_base + x * longueur_case
-        top = y_base + y * longueur_case
-        region = (left, top, longueur_case, longueur_case)
+        left =x * longueur_case
+        top =y * longueur_case
+        region = screen.crop((left, top, left + longueur_case, top + longueur_case))
         print("___________________________________")
-        print(x, y, region)
+        print(x, y)
         # screenshot(region=region).show()
         a_ajouter = " "
         for nb_fichier in files:
-            if nb_fichier in files[3:]:
-                results = locateOnScreen(f"nombres/{nb_fichier}", confidence=0.80, grayscale=True, region=region)
+            if nb_fichier in files:
+                results = locate(f"nombres/{nb_fichier}",region, confidence=0.80, grayscale=True)
                 if results is not None:
                     a_ajouter = nb_fichier[0]
                     print(a_ajouter)
