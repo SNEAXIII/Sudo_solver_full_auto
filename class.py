9e909eb89fcbb33b19e5d1b0f73a7df92cc5df33
AlexisB <misterbalise2@gmail.com>
2023-05-07 02:17:42 +0200

    generation map alpha

--- class.py
+++ class.py
@@ -1,15 +1,85 @@
 from typing import Optional
+from time import time, sleep
+from pyautogui import locateOnScreen, screenshot, locate
+from PIL import ImageGrab, Image
+import os
+import mouse
+import keyboard
 
 
 class Sudoku:
 
     def __init__(self):
         self.boxs = [Box(i) for i in range(9)]
-        self.colums = [Column(x) for x in range(9)]
+        self.columns = [Column(x) for x in range(9)]
         self.rows = [Row(y) for y in range(9)]
+        self.longueur_case = 55
+        self.numbers = os.listdir(f"{os.path.dirname(os.path.abspath(__file__))}\\nombres")[::-1]
+        self.screen = ImageGrab.grab((386, 242, 879, 735))
+        self.build()
+        self.all_blacklist()
+
+    def __str__(self):
+        str = ""
+        for row in self.rows:
+            str += "["
+            for field in row.list:
+                str += f"{field if field.value is not None else ' '}, "
+            str = str[:-2] + "]\n"
+        return str
+
+    def build(self):
+        for y in range(9):
+            for x in range(9):
+                region = self.select_region(x, y)
+                a_ajouter = None
+                if not self.is_empty_region(region):
+                    for nb in self.numbers:
+                        est_present = locate(f"nombres/{nb}", region, confidence=0.80, grayscale=True)
+                        if nb in self.numbers and est_present is not None:
+                            a_ajouter = int(nb[0])
+                            break
+                self.add_field_all(x, y, (x // 3) + (y // 3 * 3), a_ajouter)
+
+    def all_blacklist(self):
+        for row in self.rows:
+            for field in row.list:
+                if field.filled:
+                    self.blacklist(field)
+
+    def blacklist(self, field):
+        value = field.value
+        y = field.y
+        x = field.x
+        i = field.i
+        for item in self.rows[y].list:
+            if not item.filled:
+                self.rows[y].remove(value)
+        for item in self.columns[x].list:
+            if not item.filled:
+                self.columns[x].remove(value)
+        for item in self.boxs[i].dico:
+            if not item.filled:
+                self.boxs[i].remove(value)
+
+    def is_empty_region(self, region):
+        set_couleur = set()
+        for index in range(7, 20):
+            set_couleur.add(region.getpixel((25, index)))
+        return len(set_couleur) == 1
+
+    def select_region(self, x: int, y: int):
+        left, top = x * self.longueur_case, y * self.longueur_case
+        return self.screen.crop((left, top, left + self.longueur_case, top + self.longueur_case))
+
+    def add_field_all(self, x: int, y: int, i: int, value: int):
+        field = Field(x, y, i, value)
+        self.add_field_box(field)
+        self.add_field_column(field)
+        self.add_field_row(field)
 
     def add_field_box(self, field):
-        selected_box = self.boxs[(field.x // 3) + (field.y // 3 * 3)]
+        selected_box = self.boxs[field.i]
         selected_box.dico.add(field)
         if field.value is not None:
             if field.value in selected_box.white_list:
@@ -18,7 +88,7 @@ class Sudoku:
                 raise ValueError(f"The field {field} already exist the box n°{selected_box.i}")
 
     def add_field_column(self, field):
-        selected_column = self.colums[field.x]
+        selected_column = self.columns[field.x]
         selected_column.list[field.y] = field
         if field.value is not None:
             if field.value in selected_column.white_list:
@@ -35,12 +105,6 @@ class Sudoku:
             else:
                 raise ValueError(f"The field {field} already exist the row n°{selected_row.y}")
 
-    def add_field_all(self, x: int, y: int, value: int):
-        field = Field(x, y, value)
-        self.add_field_box(field)
-        self.add_field_column(field)
-        self.add_field_row(field)
-
 
 class Box:
 
@@ -51,6 +115,9 @@ class Box:
 
     def __str__(self):
         return f"{self.dico}"
+
+    def remove(self, nb: int):
+        self.white_list.discard(nb)
 
 
 class Column:
@@ -63,6 +130,9 @@ class Column:
     def __str__(self):
         return f"{self.list}"
 
+    def remove(self, nb: int):
+        self.white_list.discard(nb)
+
 
 class Row:
 
@@ -74,14 +144,17 @@ class Row:
     def __str__(self):
         return f"{self.list}"
 
+    def remove(self, nb: int):
+        self.white_list.discard(nb)
+
 
 class Field:
 
-    def __init__(self, x: int, y: int, value: Optional[int] = None):
-        self.x, self.y = x, y
+    def __init__(self, x: int, y: int, i: int, value: Optional[int] = None):
+        self.x, self.y, self.i = x, y, i
         self.value = value
         self.filled = True if self.value is not None else False
-        self.is_set = True if self.value is not None else False
+        self.is_write = True if self.value is not None else False
         self.white_list = set(range(1, 10)) if self.value is None else set()
 
     def __str__(self):
@@ -90,9 +163,6 @@ class Field:
 
 sudo = Sudoku()
 
-sudo.add_field_all(0, 0, 1)
-sudo.add_field_all(1, 0, 2)
-
-print(sudo.rows[0])
+print(sudo)
 a = 0
 a = 0
